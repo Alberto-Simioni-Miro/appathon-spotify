8663fd092b4adca2fecb1b4e56cacb399d308425
Alberto Simioni <alberto@miro.com>
2022-04-13 13:53:57 +0200

    Added Drag & Drop

--- src/app.tsx
+++ src/app.tsx
@@ -1,38 +1,56 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
-async function addSticky() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
+// async function addSticky() {
+//   const stickyNote = await miro.board.createStickyNote({
+//     content: 'Hello, World!',
+//   });
 
-  await miro.board.viewport.zoomTo(stickyNote);
+//   await miro.board.viewport.zoomTo(stickyNote);
+// }
+
+async function init() {
+  // Enable the 'drop' event on the app panel. Active on 'miro-draggable' HTML elements
+  await miro.board.ui.on('drop', async ({x, y, target}) => {
+    let text : string | undefined = undefined
+    if (target.textContent !== null){
+      text = target.textContent
+    }
+
+    addPlaylistToBoard()
+  });
 }
 
-function App() {
-  React.useEffect(() => {
-    addSticky();
-  }, []);
 
-  const addEmbeddedSpotify = async () => {
-    await miro.board.createEmbed({
-      url: 'https://open.spotify.com/embed/playlist/5830XyzOtYzFxtMJcYfjk7?utm_source=generator',
-      thumbnailUrl: '',
-      mode: 'inline',
-      width: 720,
-      height: 720,
-      x: 0,
-      y: 0,
-    });
-  }
+async function addPlaylistToBoard(){
+  await miro.board.createEmbed({
+    url: 'https://open.spotify.com/embed/playlist/5830XyzOtYzFxtMJcYfjk7?utm_source=generator',
+    thumbnailUrl: '',
+    mode: 'inline',
+    width: 720,
+    height: 720,
+    x: 0,
+    y: 0,
+  });
+}
+
+
+init(); 
+
+function App() {
+  // React.useEffect(() => {
+  //   addSticky();
+  // }, []);
+
+  
 
   return (
     <div className="grid wrapper">
       <div className="cs1 ce12">
         <a
-          className="button button-primary"
+          className="button button-primary miro-draggable"
           href="#"
-          onClick={addEmbeddedSpotify}
+          onClick={addPlaylistToBoard}
         >
           Add Spotify
         </a>
